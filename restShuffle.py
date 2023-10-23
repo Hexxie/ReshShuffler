f9732bb484a4a400efedc2291b722dbabc9084ad
Anastasiia Rusakova <arusakova917@gmail.com>
2023-10-23 17:55:42 +0300

    Filter events that were not executed today (#2)
    * Filter events that were not executed today
    
    I wanted my rest diverse so I needed to be sure that
    I will not go to the coffeeshop all day long. That's why
    I added filtering events by today.
    
    1) When event shuffling, it updates the database with new
       timestamp and increase counter
    
    2) Before event shuffles, I get all the events that I have
       as a user, and remove those who have today's date
    
    3) We're shuffling with events that were not executed today.
       If I run out of events I can go play a computer game. Yay!

--- restShuffle.py
+++ restShuffle.py
@@ -1,6 +1,8 @@
 from random import shuffle
 import sqlite3
 import argparse
+import datetime
+from dateutil import parser
 
 con = sqlite3.connect("restEvents.db")
 
@@ -16,46 +18,75 @@ def add_event(event, user_id):
     con.commit()
 
 def list_all_events():
-    res = cur.execute(f"""SELECT USER.name, REST_EVENT.name
+    res = cur.execute(f"""SELECT USER.name, REST_EVENT.name, REST_EVENT.counter, REST_EVENT.event_id, REST_EVENT.last_date
                 FROM USER INNER JOIN REST_EVENT ON USER.user_id = REST_EVENT.user_id""")
-    return [item[1] for item in res.fetchall()]
+    #return [item[1] for item in res.fetchall()]
+    return res.fetchall()
 
-
-def list_all_events_dev():
-    res = cur.execute("""SELECT USER.name, REST_EVENT.name, REST_EVENT.counter, REST_EVENT.event_id
+def list_all_events_today():
+    res = cur.execute("""SELECT USER.name, REST_EVENT.name, REST_EVENT.counter, REST_EVENT.event_id, REST_EVENT.last_date
                 FROM USER INNER JOIN REST_EVENT ON USER.user_id = REST_EVENT.user_id""")
-    print(res.fetchall())
-    
+    events = res.fetchall()
+    #Remove events with today's date from the list to exclude them from shuffling
+    for item in events.copy():
+        try:
+            date_str = item[4]
+            event_date = parser.parse(date_str)
+            if(event_date.date() == datetime.date.today()):
+                events.remove(item)
+        except TypeError:
+            continue
+    return events
 
 def remove_event(event_id, user_id):
     cur.execute(f"""DELETE FROM REST_EVENT WHERE event_id = '{event_id}' AND user_id = '{user_id}'""")
     con.commit()
 
-def get_shuffled_event():
-    all_events = list_all_events()
+def update_event(user_id, event_id, counter, date):
+    cur.execute(f"""UPDATE REST_EVENT 
+                SET counter = '{counter}', last_date = '{date}'
+                WHERE event_id = '{event_id}' AND user_id = '{user_id}'""")
+    con.commit()
+
+def get_shuffled_event(user_id):
+    all_events = list_all_events_today()
+    if not all_events:
+        return "I run out of the events. You can choose whatever you want!"
     shuffle(all_events)
-    return all_events[0]
+    chosen_event = all_events[0]
+    print(chosen_event)
+
+    #This is how to update an event with date and counter (I want to know how often I get particular events)
+    count = chosen_event[2] + 1
+    event_id = chosen_event[3]
+    event_name = chosen_event[1]
+    update_event(user_id, event_id, count, datetime.datetime.now())
+    return event_name
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-a', '--add')
-    parser.add_argument('-l', '--list', action="store_true")
-    parser.add_argument('-r', '--rest', action="store_true")
-    parser.add_argument('-d', '--delete')
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('-a', '--add')
+    arg_parser.add_argument('-l', '--list', action="store_true")
+    arg_parser.add_argument('-t', '--today', action="store_true")
+    arg_parser.add_argument('-r', '--rest', action="store_true")
+    arg_parser.add_argument('-d', '--delete')
 
-    args = parser.parse_args()
+    args = arg_parser.parse_args()
 
     #print(args)
 
     if args.list:
-        list_all_events_dev()
+        print(list_all_events())
+    
+    if args.today:
+        print(list_all_events_today())
 
     if args.add:
         add_event(args.add, 1)
 
     if args.rest:
         print()
-        print(get_shuffled_event())
+        print(get_shuffled_event(1))
 
     if args.delete:
         #provide event_id here
