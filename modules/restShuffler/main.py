e4115755fc602942b1f67480fdde75387c0d0576
Anastasiia Rusakova <arusakova917@gmail.com>
2023-11-28 22:06:05 +0200

    Work events for shuffler (#24)
    - Added a new table for work events
    - Updated Event class to work with different event types
    - Distinguish between work and rest mode in the main script
    - Added a file with events to pre-fill

--- modules/restShuffler/main.py
+++ modules/restShuffler/main.py
@@ -6,10 +6,14 @@ from rest_shuffle import Event
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser()
+    #subparsers = arg_parser.add_subparsers(help='sub-command help')
+
+    arg_parser.add_argument('--work', action="store_true")
+    arg_parser.add_argument('--rest', action="store_true")
     arg_parser.add_argument('-a', '--add')
     arg_parser.add_argument('-l', '--list', action="store_true")
     arg_parser.add_argument('-t', '--today', action="store_true")
-    arg_parser.add_argument('-r', '--rest', action="store_true")
+    arg_parser.add_argument('-g', '--get_event', action="store_true")
     arg_parser.add_argument('-d', '--delete')
     arg_parser.add_argument('-f', '--fill')
 
@@ -18,7 +22,21 @@ if __name__ == '__main__':
 
     args = arg_parser.parse_args()
 
-    events = Event()
+    if(not args.work and not args.rest):
+        arg_parser.print_help()
+        exit()
+    elif(args.work and args.rest):
+        arg_parser.print_help()
+        exit()
+
+    if args.work:
+        print("creating work event")
+        table_name = 'WORK_EVENT'
+        events = Event(1, table_name)
+    elif args.rest:
+        print("creating rest event")
+        table_name = 'REST_EVENT'
+        events = Event(1, table_name)
 
     if args.list:
         print(events.find_all())
@@ -29,9 +47,9 @@ if __name__ == '__main__':
     if args.add:
         events.add(args.add)
 
-    if args.rest:
+    if args.get_event:
         print()
-        print(rest_shuffle.get_shuffled_event(1))
+        print(rest_shuffle.get_shuffled_event(1, table_name))
 
     if args.delete:
         #provide event_id here
@@ -40,6 +58,6 @@ if __name__ == '__main__':
     if args.fill:
         if os.path.isfile(args.fill):
             print(f"File {args.fill} was found")
-            rest_shuffle.add_events_from_file(args.fill, 1)
+            rest_shuffle.add_events_from_file(args.fill, 1, table_name)
         else:
             print("File is not found")
